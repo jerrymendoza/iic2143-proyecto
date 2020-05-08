06e8437564e57dce4f1c01cb5a3267e4ddd49776
Jerry Mendoza <jamendoza@uc.cl>
2020-05-08 01:24:02 -0400

    :sparkles: Add PropietarioLocal login (devise)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :propietario_locals
   devise_for :admins
   devise_for :matchers
   resources :comunas
---
+++ test/models/propietario_local_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PropietarioLocalTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
