ecac55fb1d16e31685f168e192e46948edc69334
Vicente <viperez2@uc.cl>
2020-05-08 15:50:04 -0400

    :sparkles: CRUD Manual Gustos

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'gustos/new', to: 'gustos#new'
+  post 'gustos', to: 'gustos#create'
+  get 'gustos', to: 'gustos#index'
+  get 'gustos/:id', to: 'gustos#show', as:'gusto'
+  get 'gustos/:id/edit', to: 'gustos#edit', as:'gustos_edit'
+  patch 'gustos/:id', to: "gustos#update"
+  delete 'gustos/:id', to: "gustos#destroy"
   resources :comunas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'hello#index'
