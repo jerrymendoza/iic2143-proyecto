3de94b6e8c56e041bd1f7aa6318b3eba47d7960b
Jerry Mendoza <jamendoza@uc.cl>
2020-06-11 23:18:06 -0400

    :construction: :lipstick: Add template structure

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,3 +1,11 @@
 class HelloController < ApplicationController
-    layout 'home'
+    layout :choose_layout
+
+    def choose_layout
+        if (matcher_signed_in? || admin_signed_in? || propietario_local_signed_in?)
+          "application"
+        else
+          "home"
+        end
+      end
 end
