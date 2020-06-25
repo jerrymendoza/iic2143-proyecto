bb181981771afbccf36e87a7a3af1a5af79e9e71
Alvaro Quintana Valdivia <alvaroq@uc.cl>
2020-06-25 02:41:53 -0400

    :sparkles: Add Crud Categoria and relationships

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_075249) do
+ActiveRecord::Schema.define(version: 2020_06_25_061953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,12 @@ ActiveRecord::Schema.define(version: 2020_06_04_075249) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "nombre"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "comentarios", force: :cascade do |t|
     t.bigint "matcher_id"
     t.bigint "local_id"
@@ -70,8 +76,10 @@ ActiveRecord::Schema.define(version: 2020_06_04_075249) do
   create_table "gustos", force: :cascade do |t|
     t.string "titulo"
     t.text "descripcion"
+    t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_gustos_on_category_id"
   end
 
   create_table "gustos_matchers", force: :cascade do |t|
